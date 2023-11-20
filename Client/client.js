80f752657f2cb307993da597e61d6b567c977652
kenarrr <itskenar@gmail.com>
2023-11-19 21:08:31 -0800

    Classification Model FULLY COMPLETE.

--- Client/client.js
+++ Client/client.js
@@ -1,33 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
     const callApiButton = document.getElementById("call-api-button");
     const fileUploadInput = document.getElementById("file-upload");
-  
-    callApiButton.addEventListener("click", async () => {
-        console.log("Button clicked"); // Check if this log appears in the console
+    const resultDisplay = document.getElementById("result-display");
 
-      fileUploadInput.click(); // Trigger the file upload input when the button is clicked
+    callApiButton.addEventListener("click", async () => {
+        fileUploadInput.click();
     });
-  
+
+    // Function to handle displaying the image preview
+    function displayImage(file) {
+        const reader = new FileReader();
+
+        reader.onload = function (event) {
+            const imgPreview = document.createElement("img");
+            imgPreview.classList.add("image-preview");
+            imgPreview.src = event.target.result;
+            resultDisplay.innerHTML = ''; // Clear previous content
+            resultDisplay.appendChild(imgPreview);
+        };
+
+        reader.readAsDataURL(file); // Read the file as a data URL
+    }
+
     // Listen for changes in the file input
     fileUploadInput.addEventListener("change", async (event) => {
-      const file = event.target.files[0]; // Get the selected file
+        const file = event.target.files[0];
   
-      // Perform the API call only if a file is selected
-      if (file) {
-        try {
-          const formData = new FormData();
-          formData.append("file", file);
+        if (file) {
+            try {
+                displayImage(file); // Display the image preview
+
+                const formData = new FormData();
+                formData.append("file", file);
   
-          const response = await fetch("/call-api", {
-            method: "POST",
-            body: formData,
-          });
+                const response = await fetch("/call-api", {
+                    method: "POST",
+                    body: formData,
+                });
   
-          const result = await response.json();
-          console.log(result); // Handle the API response here
-        } catch (error) {
-          console.error("Error calling API:", error);
+                const result = await response.json();
+                console.log(result); // Handle the API response here
+
+                // Display each result item in the result display container
+                result.forEach(item => {
+                    const resultItem = document.createElement("div");
+                    resultItem.classList.add("result-item");
+                    resultItem.textContent = `Label: ${item.label}\nScore: ${item.score.toFixed(4)}`;
+                    resultDisplay.appendChild(resultItem);
+                });
+            } catch (error) {
+                console.error("Error calling API:", error);
+            }
         }
-      }
     });
-  });
+});
